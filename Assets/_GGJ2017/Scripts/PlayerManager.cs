9417293c53a7a675d719cbf557c410b8bb2bdf4f
Daniel Ferrazoli <daniel@ferrazoli.com>
2017-01-21 15:17:04 -0200

    Life bar and button overlay

--- Assets/_GGJ2017/Scripts/PlayerManager.cs
+++ Assets/_GGJ2017/Scripts/PlayerManager.cs
@@ -2,36 +2,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
-
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
 public class PlayerManager : MonoBehaviour
 {
-    public float soundAmp;
-    public Color colFail = Color.red;
-    public Color colWin = Color.green;
-    public Color colNormal = Color.white;    
+    private AudioSource audioSource;
+
+    public readonly Color colFail = Color.red;
+    public readonly Color colWin = Color.green;
 
     public Light testLight;
+
     private int teclaAtual;
-    private AudioSource audioSource;
+    private bool pressed;
 
     public float bpm;
     private float time;
 
-    private bool pressed;
+    public Text teclaAtualTexto;
+    public GameObject lifeBar;
+
+    public int maxHealth = 15;
+    private int health;
+    public int acertosTrigger = 3;
+    public int acertosConsecutivos;
 
     void Start()
     {
-        teclaAtual = getKey();     
-        pressed = false;
-        testLight.color = colNormal;
+        teclaAtual = getKey();
+        health = maxHealth;
+        pressed = false;        
         time = 60 / bpm;
         InvokeRepeating("CheckBeat", 0, time);
     }
 
     void Update()
-    { 
+    {
         if (Input.GetKeyDown(KeyCode.D))
         {
             if(teclaAtual == 0)
@@ -52,54 +59,66 @@ public class PlayerManager : MonoBehaviour
             if (teclaAtual == 3)
                 pressed = true;
         }
-
     }
 
     public void CheckBeat()
     {    
         testLight.gameObject.SetActive(true);
-        if (pressed) testLight.color = colWin;
-        else testLight.color = colFail;
+
+        if (pressed)
+        {
+            acertosConsecutivos++;
+            testLight.color = colWin;
+        }
+        else
+        {
+            acertosConsecutivos = 0;
+            testLight.color = colFail;
+            if (health > 0)
+                health--;            
+        }
+
+        if(acertosConsecutivos == 3)
+        {
+            acertosConsecutivos = 0;
+            if (health < maxHealth)
+                health++;            
+        }
+
+        lifeBar.GetComponent<RectTransform>().sizeDelta = new Vector2(10 * health, 16);
+
         pressed = false;
         teclaAtual = getKey();
-        Debug.Log(valTecla(teclaAtual));
+        teclaAtualTexto.text = valTecla().ToString();
+        Debug.Log(valTecla());
         Invoke("turnOff", 0.1f);
     }
 
     public void turnOff()
     {
-        testLight.gameObject.SetActive(false);    
+        testLight.gameObject.SetActive(false);   
     }    
 
     public void onOnbeatDetected()
     {
         CheckBeat();
     }
-
-    public void onSpectrum(float[] spectrum)
-    {
-        for (int i = 0; i < spectrum.Length; ++i)
-        {
-            Vector3 start = new Vector3(i, 0, 0);
-            Vector3 end = new Vector3(i, spectrum[i], 0);
-            Debug.DrawLine(start, end);
-        }
-    }
+   
     public int getKey()
     {
         return UnityEngine.Random.Range(0, 4);
     }
-    public char valTecla(int tecla)
+
+    public char valTecla()
     {
         if (teclaAtual == 0)
-            return 'd';
+            return 'D';
         if (teclaAtual == 1)
-            return 'f';
+            return 'F';
         if (teclaAtual == 2)
-            return 'j';
+            return 'J';
         if (teclaAtual == 3)
-            return 'k';
-        return ' '; 
+            return 'K';
+        return ' ';
     }
-
 }
