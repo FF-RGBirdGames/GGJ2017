0a314bbc989de5cc974658024d3447157b8c7a61
FF <ff@rgbirdgames.com>
2017-01-20 22:16:52 -0200

    Basico

---
+++ Assets/_GGJ2017/Scripts/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PlayerManager : MonoBehaviour 
+{
+    public float soundAmp;
+    public Color colFail = Color.red;
+    public Color colWin = Color.green;
+    public Color colNormal = Color.white;
+
+    public bool flipSwitch;
+
+    public Light testLight;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckBeat();
+        }
+    }
+
+    public void CheckBeat()
+    {
+        Debug.Log("CheckBeat Apertado");
+        flipSwitch = !flipSwitch;
+        testLight.gameObject.SetActive(!flipSwitch);
+        testLight.color = colWin;
+
+    }
+}
